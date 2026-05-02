1f60ecde4982fcf91d6df4fdedfa1e67efa264b4
arjunkumar811 <kumararjun26401@gmail.com>
2026-05-02 11:42:48 +0530

    Build Error

--- next.config.js
+++ next.config.js
@@ -6,6 +6,13 @@ const nextConfig = {
       { protocol: 'https', hostname: 'cdn.jsdelivr.net' },
       { protocol: 'https', hostname: 'assets.coingecko.com' }
     ]
+  },
+  webpack: (config) => {
+    config.ignoreWarnings = [
+      ...(config.ignoreWarnings || []),
+      { module: /virtualMasterPool\.js/, message: /Critical dependency: the request of a dependency is an expression/ }
+    ];
+    return config;
   }
 };
 
